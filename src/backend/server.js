c4ed4c3dc5508df9445f29b7e3f43ade1543bf85
Kanishk Purohit <Kanishkpurohit@Kanishks-MacBook-Pro.local>
2017-08-27 02:35:31 +1000

    Adjusting server file

--- src/backend/server.js
+++ src/backend/server.js
@@ -6,11 +6,11 @@ app.set("port", process.env.PORT || 3001);
 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static('../../build/');
 }
 
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/build/index.html');
+  response.sendFile('../../build/index.html');
 });
 
 app.get("/hello", (req, res) => {

3dd53d2badc512363635858184a67e38a6a8980b
jcharles8246 <jcharles8246@gmail.com>
2017-09-02 18:35:20 +1000

    why am I like this

--- server/server.js
+++ server/server.js
@@ -11,11 +11,34 @@ const router = express.Router()
 const staticFiles = express.static(path.join(__dirname, '../../client/build'))
 app.use(staticFiles)
 
-var request = require('request')
-router.get('/users', function(req,res) {
-  request('https://jsonplaceholder.typicode.com/users', function(error, response, body) {
-    res.json(body)
-  });
+// DOMAIN API CALL
+/*
+Note there are two sets of credentials so if one doesn't work use the other..
+client_id: 'pr7nycgdtyfw56ahvnpy9eyu',
+client_secret: '8USA7GAgzD',
+client_id2: 'mzt6yh8t2ysh7kdtrpbda2cy',
+client_secret2: 'tKSNGm4Ysh'
+*/
+
+
+// Problem -- I have no fcking idea how to implement domain api call, let alone authenticate
+// installed a OAuth2 wrapper to help me out but still getting issues
+// I hate javascript and web development
+
+var ClientOAuth2 = require('client-oauth2')
+var domainAuth = new ClientOAuth2({
+    clientId: 'pr7nycgdtyfw56ahvnpy9eyu',
+    clientSecret: '8USA7GAgzD',
+    authorizationUri: 'https://auth.domain.com.au/v1/connect/token',
+    scopes: ['api_addresslocators_read'] 
+});
+
+
+//var request = require('request')
+router.get('/auth/domain', function(req,res) {
+  var uri = domainAuth.code.getUri()
+  console.log(uri)
+  res.redirect(uri)
 });
 
 router.get('/hello', (req, res) => {

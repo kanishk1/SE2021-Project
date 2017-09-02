44aae50cd78a49b933839f0f0b585b346a5adf45
Mash <z5102072@student.unsw.edu.au>
2017-09-02 18:42:58 +1000

    omg first commit

--- server/server.js
+++ server/server.js
@@ -12,25 +12,51 @@ const staticFiles = express.static(path.join(__dirname, '../../client/build'))
 app.use(staticFiles)
 
 var request = require('request')
-router.get('/users', function(req,res) {
-  request('https://jsonplaceholder.typicode.com/users', function(error, response, body) {
-    res.json(body)
-  });
-});
 
-router.get('/hello', (req, res) => {
-  const param = req.query.q;
-  if (param) {
-    res.json({
-      name: param 
+app.get('/api', function(req, res, next) {
+
+    request("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyAu2xaFuNTQ0JQPUIXMILT1l29nuWYEO0Q", function (error, response, body) {
+      console.log('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //   console.log('body:', body); // Print the HTML for the Google homepage.
+    //   res.json(body);
+      res.send(body);
     });
-    return;
-  }
+  // request({
+  //   method: "GET",
+  //   url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyAu2xaFuNTQ0JQPUIXMILT1l29nuWYEO0Q",
+  //   function(error, response, body) {
+  //     if (!error && response.statusCode === 200) {
+  //       console.log(body);
+  //       res.json(body);
+  //       res.send(body);
+  //       res.send('hello world');
+  //     } else {
+  //       res.json(error);
+  //     }
+  //   }
+  // });
 });
 
-app.get('/test', function(req, res){
-    res.send('hello world');
-});
+// router.get('/hello', (req, res) => {
+//   const param = req.query.q;
+//   if (param) {
+//     res.json({
+//       name: param
+//     });
+//     // console.log("hello world");
+//     return;
+//   }
+// });
+
+// app.get('/test', function(req, res){
+//     var request = require('request');
+// request('http://www.google.com', function (error, response, body) {
+//  console.log('error:', error); // Print the error if one occurred
+//  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//  console.log('body:', body); // Print the HTML for the Google homepage.
+// });
+// });
 
 app.use(router)
 
@@ -41,3 +67,11 @@ app.set('port', (process.env.PORT || 3001))
 app.listen(app.get('port'), () => {
   console.log(`Listening on ${app.get('port')}`)
 })
+
+/*
+qs: {
+    locationID: '-33.8670522,151.1957362',
+    radiusID: '500',
+    api_key: 'AIzaSyAu2xaFuNTQ0JQPUIXMILT1l29nuWYEO0Q'
+},
+*/

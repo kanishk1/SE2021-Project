21764ec898d853cb50feecce4d11c060753abbab
TwilightSpectre <natattak@gmail.com>
2017-09-02 21:00:40 +1000

    Fixed format of latitude/longitude.
    Can also give command-line args for which database/collection
    to modify.
    
    This is also now the ugliest perl script I've ever written.

--- scripts/get_suburb_list.sh
+++ scripts/get_suburb_list.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 wget -q -O- "http://www.gnb.nsw.gov.au/__gnb/gnr.zip" | zcat -q - |
-perl -MText::ParseWords -ne'@f=parse_line(",",0,$_);print join(",",@f[1,11,12])."\n" if ($f[2]=~/SUBURB/)' |
-mongoimport -h "localhost:27017" -d "suburber" -c "suburb_names" -f "name,lat,long" --type "csv" --drop
+perl -MText::ParseWords -ne'@f=parse_line(",",0,$_);next if$f[2]!~/SUBURB/;
+    for$i(@f[11,12]){$i=~/(-)?(\d+) +(\d+) +(\d+)/;$i=$2+($3+$4/60)/60;$i*=-1if$1}
+    print join(",",@f[1,11,12])."\n"' |
+mongoimport -h "${1:-localhost:27017}" -d "${2:-suburber}" -c "suburb_names" -f "name,lat,long" --type "csv" --drop

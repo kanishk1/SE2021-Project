369469ca939e67e6878d86136a82cc040833b03d
TwilightSpectre <natattak@gmail.com>
2017-09-07 01:31:06 +1000

    Script should now work properly for everyone.

--- scripts/get_suburb_list.sh
+++ scripts/get_suburb_list.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-wget -q -O- "http://www.gnb.nsw.gov.au/__gnb/gnr.zip" | zcat -q - |
+tmp=$(mktemp)
+trap "rm -f ${tmp}" INT TERM HUP EXIT
+
+wget -O${tmp} "http://www.gnb.nsw.gov.au/__gnb/gnr.zip"
+
+unzip -qp ${tmp} |
 perl -MText::ParseWords -ne'@f=parse_line(",",0,$_);next if$f[2]!~/SUBURB/;
     for$i(@f[11,12]){$i=~/(-)?(\d+) +(\d+) +(\d+)/;$i=$2+($3+$4/60)/60;$i*=-1if$1}
     print join(",",@f[1,11,12])."\n"' |
 mongoimport -h "${1:-localhost:27017}" -d "${2:-suburber}" -c "suburb_names" -f "name,lat,long" --type "csv" --drop
+
+rm ${tmp}

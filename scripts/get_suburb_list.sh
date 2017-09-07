0b7225e1817e1588bcf1117373b2e6ea24d8fd1f
TwilightSpectre <natattak@gmail.com>
2017-09-07 15:48:44 +1000

    Clean up temp files when done (better)

--- scripts/get_suburb_list.sh
+++ scripts/get_suburb_list.sh
@@ -7,3 +7,5 @@ wget -O${tmp} "http://www.gnb.nsw.gov.au/__gnb/gnr.zip"
 
 unzip -qp ${tmp} | ./decode_csv.pl | ./postcode.py |
 mongoimport -h "${1:-localhost:27017}" -d "${2:-suburber}" -c "suburb_names" -f "name,lat,long,post" --type "csv" --drop
+
+rm -f ${tmp}

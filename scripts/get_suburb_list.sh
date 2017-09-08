9bf761b3de54fdba63bbda9f2ac79c73740187f7
TwilightSpectre <natattak@gmail.com>
2017-09-08 13:38:56 +1000

    Suburb list update (#4)
    Script should now work properly for everyone. Pulls all suburbs, gets postcodes for them from Google Places, and stores them into a mongo database. 
    
    Takes ~25 minutes to do a full run. Currently misses a couple of suburbs, but who really cares?

--- scripts/get_suburb_list.sh
+++ scripts/get_suburb_list.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-wget -q -O- "http://www.gnb.nsw.gov.au/__gnb/gnr.zip" | zcat -q - |
-perl -MText::ParseWords -ne'@f=parse_line(",",0,$_);next if$f[2]!~/SUBURB/;
-    for$i(@f[11,12]){$i=~/(-)?(\d+) +(\d+) +(\d+)/;$i=$2+($3+$4/60)/60;$i*=-1if$1}
-    print join(",",@f[1,11,12])."\n"' |
-mongoimport -h "${1:-localhost:27017}" -d "${2:-suburber}" -c "suburb_names" -f "name,lat,long" --type "csv" --drop
+tmp=$(mktemp)
+trap "if [ -e ${tmp} ]; then rm -f ${tmp}; fi" INT TERM HUP EXIT
+
+wget -O${tmp} "http://www.gnb.nsw.gov.au/__gnb/gnr.zip"
+
+unzip -qp ${tmp} | ./decode_csv.pl | ./postcode.py |
+mongoimport -h "${1:-localhost:27017}" -d "${2:-suburber}" -c "suburb_names" -f "name,lat,long,post" --type "csv" --drop
+
+rm -f ${tmp}

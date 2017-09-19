ec2bb2df2f58f7650cda0002f3368cf792445b7d
Mash <z5102072@student.unsw.edu.au>
2017-09-20 06:22:46 +1000

    social & news page

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -4,20 +4,24 @@ import { Tab, Tabs } from 'react-bootstrap';
 import Housing from '../components/Housing.js'
 import Demographics from '../components/Demographics.js'
 import Lifestyle from '../components/Lifestyle.js'
+import Social from '../components/Social.js'
 
 class Results extends Component {
 
   render () {
     return (
       <Tabs id="Introduction Tab" defaultActiveKey={1} >
-        <Tab eventKey={1} title="Introduction"> 
-          <Housing /> 
+        <Tab eventKey={1} title="Introduction">
+          <Housing />
         </Tab>
-        <Tab eventKey={2} title="Housing"> 
-          <Demographics /> 
+        <Tab eventKey={2} title="Housing">
+          <Demographics />
         </Tab>
-        <Tab eventKey={3} title="Demographics"> 
-          <Lifestyle /> 
+        <Tab eventKey={3} title="Demographics">
+          <Lifestyle />
+        </Tab>
+        <Tab eventKey={4} title="Social">
+          <Social />
         </Tab>
       </Tabs>
     )
@@ -26,4 +30,3 @@ class Results extends Component {
 }
 
 export default Results;
-

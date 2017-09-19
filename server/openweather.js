6829497ad1c8082112b1b6ba443e32c30f475d16
TwilightSpectre <natattak@gmail.com>
2017-09-19 23:15:23 +1000

    Parsed weather.
    Let me know if there's anything else you guys want.

--- server/openweather.js
+++ server/openweather.js
@@ -1,36 +1,78 @@
+'use strict';
+
 import express from 'express';
 import openweather from 'node-openweather';
 
 const router = express.Router();
 
-function doAPI(postcode, country) {
-    if (!postcode || !country)
-        return Promise.reject("Invalid Parameters.");
+const FORECASTS_PER_DAY = 24 / 3;
 
-    var weather = openweather({
-        key: 'aa69bf79cee1f390f63d8203bd191e3b',
-        accuracy: "like",
-        unit: "metric",
-        language: "en"
-    });
-
-    return weather.zip(postcode, country).forecast(5)
-      .then(function(result) {
-        //result['main']['temp'] -= 273.15;
-        //result['main']['temp_min'] -= 273.15;
-        //result['main']['temp_max'] -= 273.15;
-        return result;
-      });
-}
-
-// Open Weather API
-// Example Call... http://localhost:3001/weather/2220
 router.get('/:postcode', (req, res) => {
   const postcode = req.params.postcode;
   const country = 'Australia';
-  doAPI(postcode,country)
-    .then(response => res.json(response))
-    .catch(fail => res.send("Error: " + fail))
-});
+
+  if (!postcode) {
+    res.send("Error: Invalid Paramenters.");
+    return;
+  }
+
+  const weather = openweather({
+    key: 'aa69bf79cee1f390f63d8203bd191e3b',
+    accuracy: "like",
+    units: "metric",
+    language: "en"
+  });
+
+  // disable console.log because openweather annoys me
+  const logger = console.log;
+  console.log = () => {};
+
+  const promise = weather.zip(postcode, country).forecast(5)
+
+  console.log = logger; // re-enable it now ;)
+
+  promise.then((result) => {
+    const values = [];
+
+    let min = +1e99; let min_dt = null;
+    let max = -1e99; let max_dt = null;
+    let humidity = 0;
+    let conditions = "Hello";
+
+    let num_forecasts_inspected = 0;
+    for (const forecast of result['list']) {
+      if (num_forecasts_inspected === 0) {
+        conditions = forecast['weather'][0]['description'];
+      }
+
+      if (forecast['main']['temp'] > max) {
+        max = forecast['main']['temp'];
+        max_dt = new Date((forecast['dt'] + 36000) * 1000);
+      }
+      if (forecast['main']['temp'] < min) {
+        min = forecast['main']['temp'];
+        min_dt = new Date((forecast['dt'] + 36000) * 1000);
+      }
+      humidity += forecast['main']['humidity'];
+
+      if (++num_forecasts_inspected >= FORECASTS_PER_DAY) {
+        humidity /= num_forecasts_inspected;
+        values.push({ min, min_dt, max, max_dt, humidity, conditions });
+
+        min = +1e99;
+        max = -1e99;
+        humidity = 0;
+        num_forecasts_inspected = 0;
+      }
+    }
+
+    if (num_forecasts_inspected > FORECASTS_PER_DAY/2) {
+      humidity /= num_forecasts_inspected;
+      values.push({ min, min_dt, max, max_dt, humidity, conditions });
+    }
+
+    res.json(values);
+  }).catch(err => res.send("Error: " + err));
+})
 
 export default router;

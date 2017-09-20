242431b44445b0937b64cd6d13566dbe8082de2f
TwilightSpectre <natattak@gmail.com>
2017-09-20 15:04:26 +1000

    Parsed bing news.
    ... There wasn't actually much to do. I don't really know what you
    guys want, anyway; basically you're getting what's there, minus
    some random useless junk.

--- server/bing.js
+++ server/bing.js
@@ -1,33 +1,42 @@
 import express from 'express';
+import bing from 'node-bing-api';
 
 const router = express.Router();
 
-function doAPI(suburb, numResults) {
-    if (!suburb || !numResults)
-        return Promise.reject("Invalid Parameters");
-
-    return new Promise((response, fail) => {
-        var apikey1 = '2e466941fe314289af733fa47696b09c';
-        var apikey2 = '39937d23b2784b3584ce3252c1c11e44';
-        var Bing = require('node-bing-api')({ accKey: apikey1 });
-        Bing.news(suburb, {
-            count: numResults,
-            market: 'en-AU',
-            adult: 'Strict'
-        }, function(error, res, body) {
-            response(JSON.stringify(body,null,2))
-        });
-    });
-}
-
-// Bing API
 // Example Call... http://localhost:3001/bing/search?suburb=hurstville&num=10
 router.get('/search', (req, res) => {
-    const suburb = req.query.suburb;
-    const numResults = req.query.num;
-    doAPI(suburb, numResults)
-        .then(response => res.end(response))
-        .catch(fail => res.send(fail))
+  const suburb = req.query.suburb;
+  const numResults = req.query.num;
+
+  if (!suburb || !numResults) {
+    res.send("Error: Invalid Parameters");
+    return;
+  }
+
+  const apikey1 = '2e466941fe314289af733fa47696b09c';
+  const apikey2 = '39937d23b2784b3584ce3252c1c11e44';
+  bing({ accKey: apikey1 }).news(suburb, {
+    count: numResults,
+    market: 'en-AU',
+    adult: 'Strict'
+  }, (error, data, body) => {
+    if (error)
+      res.send("Error: " + error);
+    else {
+      const results = [];
+      for (const article of body['value']) {
+        results.push({
+          name:        article['name'],
+          url:         article['url'],
+          image:       article['image']['thumbnail'], // provides url,width,height
+          description: article['description'],
+          publishDate: article['datePublished'],
+          category:    article['category'] // won't always get this
+        });
+      }
+      res.json(results);
+    }
+  });
 });
 
 export default router;

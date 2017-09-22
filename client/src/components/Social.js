e2b9443b742bc5386fd7e85b9c0328b7b2fededa
Mash <z5102072@student.unsw.edu.au>
2017-09-22 10:44:50 +1000

    social hardcoded

--- client/src/components/Social.js
+++ client/src/components/Social.js
@@ -1,130 +1,37 @@
 import React, { Component } from 'react';
-import {Grid, Row, Col, Thumbnail, Tabs, Tab, Button} from 'react-bootstrap';
-import placeholder from '../img/placeholder.png';
-import News from './News.js'
+// import {Grid, Row, Col, Thumbnail, Button} from 'react-bootstrap';
+// import placeholder from '../img/placeholder.png';
+import insta1 from '../img/insta1.png'
+import insta2 from '../img/insta2.png'
+import insta3 from '../img/insta3.png'
+
+import twat1 from '../img/twat1.png'
+import twat2 from '../img/twat2.png'
+import twat3 from '../img/twat3.png'
+import twat4 from '../img/twat4.png'
+// import InstagramEmbed from 'react-social-embed'
+// import TweetEmbed from 'react-social-embed'
+import { Image } from 'react-bootstrap'
+import SplitterLayout from 'react-splitter-layout';
 
 class Social extends Component {
   render(){
   return (
-    <Grid className="social-news" fluid={true}>
-      <Row>
-        <Col lgOffset={1} lg={10}>
-          <h1>Social</h1>
-          <div>
-            <Tabs id="social-tabs" defaultActiveKey={2} >
-              <Tab eventKey={1} title="Facebook">
-                <Row>
-                  <Col lg={4}>
-                    <Thumbnail src={placeholder} alt="242x200">
-                      <h3>Post 1</h3>
-                      <p>Description</p>
-                      <p>
-                        <Button bsStyle="primary">Button</Button>&nbsp;
-                        <Button bsStyle="default">Button</Button>
-                      </p>
-                    </Thumbnail>
-                  </Col>
-                  <Col lg={4}>
-                    <Thumbnail src={placeholder} alt="242x200">
-                      <h3>Post 2</h3>
-                      <p>Description</p>
-                      <p>
-                        <Button bsStyle="primary">Button</Button>&nbsp;
-                        <Button bsStyle="default">Button</Button>
-                      </p>
-                    </Thumbnail>
-                  </Col>
-                  <Col lg={4}>
-                    <Thumbnail src={placeholder} alt="242x200">
-                      <h3>Post 3</h3>
-                      <p>Description</p>
-                      <p>
-                        <Button bsStyle="primary">Button</Button>&nbsp;
-                        <Button bsStyle="default">Button</Button>
-                      </p>
-                    </Thumbnail>
-                  </Col>
-                </Row>
-              </Tab>
-              <Tab eventKey={2} title="Twitter">
-                <Row>
-                  <Col lg={4}>
-                    <Thumbnail src={placeholder} alt="242x200">
-                      <h3>Post 1</h3>
-                      <p>Description</p>
-                      <p>
-                        <Button bsStyle="primary">Button</Button>&nbsp;
-                        <Button bsStyle="default">Button</Button>
-                      </p>
-                    </Thumbnail>
-                  </Col>
-                  <Col lg={4}>
-                    <Thumbnail src={placeholder} alt="242x200">
-                      <h3>Post 2</h3>
-                      <p>Description</p>
-                      <p>
-                        <Button bsStyle="primary">Button</Button>&nbsp;
-                        <Button bsStyle="default">Button</Button>
-                      </p>
-                    </Thumbnail>
-                  </Col>
-                  <Col lg={4}>
-                    <Thumbnail src={placeholder} alt="242x200">
-                      <h3>Post 3</h3>
-                      <p>Description</p>
-                      <p>
-                        <Button bsStyle="primary">Button</Button>&nbsp;
-                        <Button bsStyle="default">Button</Button>
-                      </p>
-                    </Thumbnail>
-                  </Col>
-                </Row>
-              </Tab>
-              <Tab eventKey={3} title="Instagram">
-                <Row>
-                  <Col lg={4}>
-                    <Thumbnail src={placeholder} alt="242x200">
-                      <h3>Post 1</h3>
-                      <p>Description</p>
-                      <p>
-                        <Button bsStyle="primary">Button</Button>&nbsp;
-                        <Button bsStyle="default">Button</Button>
-                      </p>
-                    </Thumbnail>
-                  </Col>
-                  <Col lg={4}>
-                    <Thumbnail src={placeholder} alt="242x200">
-                      <h3>Post 2</h3>
-                      <p>Description</p>
-                      <p>
-                        <Button bsStyle="primary">Button</Button>&nbsp;
-                        <Button bsStyle="default">Button</Button>
-                      </p>
-                    </Thumbnail>
-                  </Col>
-                  <Col lg={4}>
-                    <Thumbnail src={placeholder} alt="242x200">
-                      <h3>Post 3</h3>
-                      <p>Description</p>
-                      <p>
-                        <Button bsStyle="primary">Button</Button>&nbsp;
-                        <Button bsStyle="default">Button</Button>
-                      </p>
-                    </Thumbnail>
-                  </Col>
-                </Row>
-              </Tab>
-            </Tabs>
-          </div>
-        </Col>
-      </Row>
-      <Row className="news-carousel">
-        <Col lgOffset={1} lg={12}>
-          <h1>News</h1>
-          <News />
-        </Col>
-      </Row>
-    </Grid>
+    <SplitterLayout percentage>
+      <div className="my-pane">
+        <Image src={twat1} responsive />
+        <Image src={twat2} responsive />
+        <Image src={twat3} responsive />
+        <Image src={twat4} responsive />
+      </div>
+      <div className="my-pane">
+        <Image src={insta1} responsive />
+        <br> </br>
+        <Image src={insta2} responsive />
+        <br> </br>
+        <Image src={insta3} responsive />
+      </div>
+    </SplitterLayout>
     )
   }
 }

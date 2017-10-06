744285a7235462c8057065af8e46d3ff630b71d6
jcharles8246 <jcharles8246@gmail.com>
2017-10-06 11:54:37 +1100

    I didnt actually change it correctly, fixing twitter api properly?

--- server/twitter.js
+++ server/twitter.js
@@ -17,12 +17,12 @@ function doAPI(suburb, numtweets) {
             // Data parsed...
             data = JSON.parse(data);
             var statuses = data['statuses'];
+
             // We are now to extract the text from each tweet
             var tweets = [];
             var i = 0;
             for (i = 0; i < statuses.length && i < numtweets; i++) {
-                tweets.push(statuses[i]);
-                tweets.push('\n');
+                tweets.push(statuses[i]['id_str']);
             }
             response(tweets);
         }

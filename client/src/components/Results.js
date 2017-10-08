8ed4456576b0c34027be5dae3d7ac9f24726d1f4
Md Mashiur Rahman <Mash36@users.noreply.github.com>
2017-10-08 15:22:53 +1100

    Revert "Develop Social Page"

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -4,7 +4,8 @@ import Introduction from './Introduction.js'
 import Demographics from '../components/Demographics.js'
 import Lifestyle from '../components/Lifestyle.js'
 import housing from '../img/resultPage4.png'
-import Social from '../components/Social.js'
+import social from '../img/resultPage5.png'
+import news from '../img/resultPage6.png'
 
 class Results extends Component {
 
@@ -26,8 +27,8 @@ class Results extends Component {
       <div>
       <Tabs id="Introduction Tab" activeKey={this.state.key}
           onSelect={this.handleSelect}>
-          <Tab eventKey={1} title="Introduction">
-            <Introduction
+          <Tab eventKey={1} title="Introduction"> 
+            <Introduction 
               wiki={this.props.data[10]}
               name={this.props.suburbName}
               postcode={this.props.suburbPostcode}
@@ -35,25 +36,25 @@ class Results extends Component {
               weather={this.props.data[3]}
               />
           </Tab>
-          <Tab eventKey={2} title="Demographics">
-            <Demographics data={this.props.data[1]}/>
+          <Tab eventKey={2} title="Demographics"> 
+            <Demographics data={this.props.data[1]}/> 
           </Tab>
-          <Tab eventKey={3} title="Lifestyle">
-             <Lifestyle schools={this.props.data[4]}
+          <Tab eventKey={3} title="Lifestyle"> 
+             <Lifestyle schools={this.props.data[4]} 
               shops={this.props.data[5]}
-              food={this.props.data[6]}
+              food={this.props.data[6]} 
               recreation={this.props.data[7]}
               religious={this.props.data[8]}
-              wiki={this.props.data[10]} />
+              wiki={this.props.data[10]} /> 
           </Tab>
           <Tab eventKey={4} title = "Housing">
-          <img src={housing} alt="" height="100%" width="100%"/>
+          <img src={housing} alt="" height="100%" width="100%"/>            
           </Tab>
           <Tab eventKey={5} title = "Social">
-            <Social
-              news={this.props.data[2]}
-              twitter={this.props.data[9]}
-            />
+            <img src={social} alt="" height="100%" width="100%"/>
+          </Tab>
+          <Tab eventKey={6} title = "News">
+            <img src={news} alt="" height="100%" width="100%"/>
           </Tab>
       </Tabs>
       </div>

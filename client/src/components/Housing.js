983f6b7065df921845606bf2fb93d749f93512d2
jcharles8246 <jcharles8246@gmail.com>
2017-10-10 10:03:08 +1100

    Housing page is to a standard I am happy with, need Neil to complete CSS to make it look nice and pwetty <3

--- client/src/components/Housing.js
+++ client/src/components/Housing.js
@@ -45,8 +45,8 @@ class Housing extends Component {
       dots: true,
       infinite: true,
       speed: 500,
-      slidesToShow: 5,
-      slidesToScroll: 5,
+      slidesToShow: 3,
+      slidesToScroll: 3,
       nextArrow: <SampleNextArrow />,
       prevArrow: <SamplePrevArrow />
     };
@@ -57,7 +57,7 @@ class Housing extends Component {
                         <Thumbnail src={value.media.url} >
                           <h2>{value.address}</h2>
                             <p>
-                              <img src={bed} alt="photo1"></img> {value.bathrooms}
+                              <img src={bath} alt="photo1"></img> {value.bathrooms}
                               <img src={car} alt="photo2"></img> {value.carspaces}
                               <img src={bed} alt="photo3"></img> {value.bedrooms}
                             </p>
@@ -125,44 +125,11 @@ class Housing extends Component {
               </Col>
             </Row>
             <Row className="housingCarouselrow">
-              <Col className="housingCarouselCol" lg={12}>
+              <Col className="housingCarouselCol" lg={8}>
                 {this.createCards()}
               </Col>
             </Row>
           </Grid>
-            //~ <Col className="housingLeft" lg={6}>
-              //~ <Row className="housingSummary">
-                //~ {this.createCards()}
-              //~ </Row>
-              //~ <Row className="housingImage">
-                //~ <img src="" alt="pls"></img>
-              //~ </Row>
-            //~ </Col>
-            //~ <Col className="housingRight" lg={6}>
-              //~ <Row className="prices">
-                //~ <Col className="buy" lg={3}>
-                  //~ <p> pricesbuy </p>
-                //~ </Col>
-                //~ <Col className="rent" lg={3}>
-                  //~ <p> pricesrent </p>
-                //~ </Col>
-              //~ </Row>
-              //~ <Row className="photos">
-                //~ <Col className="photo1" lg={2}>
-                  //~ <img src="" alt="photos1"></img>
-                //~ </Col>
-                //~ <Col className="photo2" lg={2}>
-                  //~ <img src="" alt="photos2"></img>
-                //~ </Col>
-                //~ <Col className="photo3" lg={2}>
-                  //~ <img src="" alt="photos3"></img>
-                //~ </Col>
-                //~ <Col className="photo4" lg={2}>
-                  //~ <img src="" alt="photos4"></img>
-                //~ </Col>
-              //~ </Row>
-            //~ </Col>
-          //~ </Grid>
         )
     }
   }
